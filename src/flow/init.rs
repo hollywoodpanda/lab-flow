9ae5d8dccb7c6da9fd80d31f622a5272e0c1b836
Daniel Sampaio <daniel.sampaio@accurate.com.br>
2022-12-17 17:21:01 -0300

    [FEATURE][11-use-git-config] Reimplementing the initial script and using it in the main file.

--- src/flow/init.rs
+++ src/flow/init.rs
@@ -1,5 +1,3 @@
-use std::process::Output;
-
 use regex::Regex;
 use crate::config::constants::BRANCH_ALREADY_EXISTS_SUFFIX;
 use crate::config::store::{Store};
@@ -12,113 +10,96 @@ pub const RELEASE_BRANCH_NAME_KEY: &str = "lab.flow.branch.release";
 pub const DEVELOP_BRANCH_NAME_KEY: &str = "lab.flow.branch.develop";
 pub const MAIN_BRANCH_NAME_KEY: &str = "lab.flow.branch.main";
 
-///
-/// The key and value of a configuration.
+pub enum Script {}
+
+impl Script {
+
+    ///
+/// Checks if the repository is already initiated.
+/// It checks if the feature, bugfix, hotfix, release,
+/// develop and main branch names have values in the store.
 /// 
-#[derive(Debug)]
-#[deprecated]
-pub struct Configuration<'a> {
-    pub key: &'a str,
-    pub value: &'a str,
-}
+pub fn is_initiated () -> bool {
 
-#[derive(Debug)]
-pub struct InitError {
-    pub message: String,
-}
-
-impl InitError {
-    pub fn new(message: String) -> InitError {
-        InitError {
-            message
-        }
-    }
-}
-
-impl std::error::Error for InitError {}
-
-impl std::fmt::Display for InitError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-fn is_already_exists_message (message: &str) -> bool {
-    message.contains(BRANCH_ALREADY_EXISTS_SUFFIX)
-}
-
-fn is_given_branch_name_valid(branch_name: &str) -> bool {
-
-    let valid_branch_name_regex: Regex = match Regex::new(r"^[A-Za-z]{1,}[A_Za-z0-9_]*[A-Za-z0-9]{1,}$") {
-
-        Ok(output) => output,
+    // 1. Checking if the feature branch name is stored.
+    match Store::get(FEATURE_BRANCH_NAME_KEY) {
+        Ok(_) => {},
         Err(_) => return false
+    }
 
+    // 2. Checking if the bugfix branch name is stored.
+    match Store::get(BUGFIX_BRANCH_NAME_KEY) {
+        Ok(_) => {},
+        Err(_) => return false
+    }
+
+    // 3. Checking if the hotfix branch name is stored.
+    match Store::get(HOTFIX_BRANCH_NAME_KEY) {
+        Ok(_) => {},
+        Err(_) => return false
+    }
+
+    // 4. Checking if the release branch name is stored.
+    match Store::get(RELEASE_BRANCH_NAME_KEY) {
+        Ok(_) => {},
+        Err(_) => return false
+    }
+
+    // 5. Checking if the develop branch name is stored.
+    match Store::get(DEVELOP_BRANCH_NAME_KEY) {
+        Ok(_) => {},
+        Err(_) => return false
+    }
+
+    // 6. Checking if the main branch name is stored.
+    match Store::get(MAIN_BRANCH_NAME_KEY) {
+        Ok(_) => {},
+        Err(_) => return false
+    }
+
+    return true
+
+}
+
+pub fn show () {
+
+    let feature_branch_name = match Store::get(FEATURE_BRANCH_NAME_KEY) {
+        Ok(value) => value,
+        Err(_) => String::from("[NOT FOUND]")
     };
 
-    valid_branch_name_regex.is_match(branch_name)
+    let bugfix_branch_name = match Store::get(BUGFIX_BRANCH_NAME_KEY) {
+        Ok(value) => value,
+        Err(_) => String::from("[NOT FOUND]")
+    };
 
-}
+    let hotfix_branch_name = match Store::get(HOTFIX_BRANCH_NAME_KEY) {
+        Ok(value) => value,
+        Err(_) => String::from("[NOT FOUND]")
+    };
 
-fn read_branch_name (
-    message: &str,
-    default_branch_name: &str,
-    add_suffix_slash: bool,
-    already_used_names: &Vec<String>,
-) -> Result<String, Box<dyn std::error::Error>> {
+    let release_branch_name = match Store::get(RELEASE_BRANCH_NAME_KEY) {
+        Ok(value) => value,
+        Err(_) => String::from("[NOT FOUND]")
+    };
 
-    let mut branch_name = String::new();
+    let develop_branch_name = match Store::get(DEVELOP_BRANCH_NAME_KEY) {
+        Ok(value) => value,
+        Err(_) => String::from("[NOT FOUND]")
+    };
 
-    // We loop here so that if the user enters a branch name
-    // that is already taken, we keep asking for a new one.
-    loop {
+    let main_branch_name = match Store::get(MAIN_BRANCH_NAME_KEY) {
+        Ok(value) => value,
+        Err(_) => String::from("[NOT FOUND]")
+    };
 
-        // 1. Show the given message to the user.
-        println!("{}", message);
+    println!("The feature branch prefix is {}", feature_branch_name);
+    println!("The bugfix branch prefix is {}", bugfix_branch_name);
+    println!("The hotfix branch prefix is {}", hotfix_branch_name);
+    println!("The release branch prefix is {}", release_branch_name);
+    println!("The develop branch name is {}", develop_branch_name);
+    println!("The main branch name is {}", main_branch_name);
 
-        // 2. Read the user input.
-        match std::io::stdin().read_line(&mut branch_name) {
-            Ok(_) => {},
-            Err(e) => return Err(Box::new(e))
-        }
-
-        // 3. Remove the trailing newline character.
-        branch_name = branch_name.trim().to_string();
-
-        // 4. If the user did not enter anything, we use the default branch name.
-        if branch_name.is_empty() {
-            branch_name = default_branch_name.to_string();
-        }
-
-        // 5. If the user entered a branch name that is already taken, we keep asking for a new one.
-        if already_used_names.contains(&branch_name) {
-            println!("The branch name '{}' is already taken. Please enter a new one.", branch_name);
-            branch_name = String::new();
-            continue;
-        }
-
-        // 6. Validating if the given name is acceptable.
-        if ! is_given_branch_name_valid(&branch_name) {
-            println!("The branch name '{}' is not valid. Please enter a new one.", branch_name);
-            branch_name = String::new();
-            continue;
-        }
-
-
-        // 7. If the user entered a branch name that is already 
-        // taken, we keep asking for a new one.
-        if add_suffix_slash && !branch_name.ends_with("/") {
-            branch_name = branch_name + "/";
-        }
-
-        // 8. If we got here it means we have a valid 
-        // name for the branch. We can escape.
-        break;
-
-    }
-
-    Ok(branch_name)
-    
 }
 
 pub fn create () -> Result<(), Box<dyn std::error::Error>> {
@@ -199,6 +180,239 @@ pub fn create () -> Result<(), Box<dyn std::error::Error>> {
     used_names.clear();
 
     // 14. Creating and using the main branch
+    match create_branch_main(&main_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(e)
+    }
+
+    // 15. Pushing the main branch to the remote repository.
+    push_branch(&main_branch_name);
+
+    // 16. Initial commit at the main branch.
+    match stage_and_commit_all_files() {
+        Ok(_) => {},
+        Err(e) => return Err(e)
+    }
+
+    // 17. Creating and using the develop branch.
+    match create_branch_develop(&develop_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(e)
+    }
+
+    // 18. Pushing the develop branch to the remote repository.
+    push_branch(&develop_branch_name);
+
+    // 19. Store the branch names.
+    match store_branch_names(
+        &feature_branch_name,
+        &bugfix_branch_name,
+        &hotfix_branch_name,
+        &release_branch_name,
+        &develop_branch_name,
+        &main_branch_name
+    ) {
+        Ok(_) => {},
+        Err(e) => return Err(e)
+    }
+    
+    // 20. Operation successfull!
+    Ok(())
+
+}
+
+}
+
+#[derive(Debug)]
+pub struct InitError {
+    pub message: String,
+}
+
+impl InitError {
+    pub fn new(message: String) -> InitError {
+        InitError {
+            message
+        }
+    }
+}
+
+impl std::error::Error for InitError {}
+
+impl std::fmt::Display for InitError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+fn is_already_exists_message (message: &str) -> bool {
+    message.contains(BRANCH_ALREADY_EXISTS_SUFFIX)
+}
+
+fn is_given_branch_name_valid(branch_name: &str) -> bool {
+
+    let valid_branch_name_regex: Regex = match Regex::new(r"^[A-Za-z]{1,}[A_Za-z0-9_]*[A-Za-z0-9]{1,}[/]{0,1}$") {
+
+        Ok(output) => output,
+        Err(_) => return false
+
+    };
+
+    valid_branch_name_regex.is_match(branch_name)
+
+}
+
+fn read_branch_name (
+    message: &str,
+    default_branch_name: &str,
+    add_suffix_slash: bool,
+    already_used_names: &Vec<String>,
+) -> Result<String, Box<dyn std::error::Error>> {
+
+    let mut branch_name = String::new();
+
+    // We loop here so that if the user enters a branch name
+    // that is already taken, we keep asking for a new one.
+    loop {
+
+        // 1. Show the given message to the user.
+        println!("{}", message);
+
+        // 2. Read the user input.
+        match std::io::stdin().read_line(&mut branch_name) {
+            Ok(_) => {},
+            Err(e) => return Err(Box::new(e))
+        }
+
+        // 3. Remove the trailing newline character.
+        branch_name = branch_name.trim().to_string();
+
+        // 4. If the user did not enter anything, we use the default branch name.
+        if branch_name.is_empty() {
+            branch_name = default_branch_name.to_string();
+        }
+
+        // 5. If the user entered a branch name that is already taken, we keep asking for a new one.
+        if already_used_names.contains(&branch_name) {
+            println!("The branch name '{}' is already taken. Please enter a new one.", branch_name);
+            branch_name = String::new();
+            continue;
+        }
+
+        // 6. Validating if the given name is acceptable.
+        if ! is_given_branch_name_valid(&branch_name) {
+            println!("The branch name '{}' is not valid. Please enter a new one.", branch_name);
+            branch_name = String::new();
+            continue;
+        }
+
+
+        // 7. If the user entered a branch name that is already 
+        // taken, we keep asking for a new one.
+        if add_suffix_slash && !branch_name.ends_with("/") {
+            branch_name = branch_name + "/";
+        }
+
+        // 8. Removing the trailing slash if the user entered one
+        // and we shouldn't accept it.
+        if !add_suffix_slash && branch_name.ends_with("/") {
+            branch_name = branch_name.trim_end_matches("/").to_string();
+        }
+
+        // 9. If we got here it means we have a valid 
+        // name for the branch. We can escape.
+        break;
+
+    }
+
+    Ok(branch_name)
+    
+}
+
+fn stage_and_commit_all_files () -> Result<(), Box<InitError>> {
+
+    match GitV2::add(vec![".".to_string()]) {
+
+        Ok(_) => {
+
+            // We need to commit the staged files.
+            match GitV2::commit(String::from("Initial commit")) {
+                Ok(_) => {},
+                Err(e) => {
+
+                    println!("We could not commit the staged files. Please do it manually. Error: {}", e);
+
+                }
+            };
+
+        },
+        Err(e) => {
+
+            let error_message = format!("We could not stage all files. Please do it manually. Error: {}", e);
+
+            println!("{}", &error_message);
+
+            return Err(Box::new(InitError::new(error_message)));
+
+        }
+
+    }
+
+    Ok(())
+
+}
+
+fn push_branch (branch_name: &str) {
+
+    match GitV2::push(branch_name) {
+        Ok(_) => {},
+        Err(_) => {
+
+            println!("We could not push the branch '{}' to the remote repository. Please do it manually.", branch_name);
+
+        }
+    };
+}
+
+fn create_branch_develop (develop_branch_name: &str) -> Result<(), Box<InitError>> {
+
+    match GitV2::branch(Option::None, develop_branch_name) {
+        Ok(_) => {
+
+            match GitV2::checkout(Option::None, develop_branch_name, false) {
+                Ok(_) => {},
+                Err(e) => {
+
+                    let error_message = format!("We could not checkout to the develop branch. Error: {}", e);
+
+                    eprintln!("{}", &error_message);
+
+                    return Err(Box::new(InitError::new(error_message)));
+
+                }
+            }
+
+        },
+        Err(err) => {
+
+            let error_message = format!("We could not create the develop branch. Error: {}", err);
+
+            eprintln!("{}", &error_message);
+
+            if ! is_already_exists_message(&err) {
+
+                return Err(Box::new(InitError::new(error_message)));
+
+            }
+
+        }
+    }
+
+    Ok(())
+
+}
+
+fn create_branch_main (main_branch_name: &str) -> Result<(), Box<InitError>> {
+
     match GitV2::checkout(
         Option::None,
         &main_branch_name,
@@ -217,15 +431,69 @@ pub fn create () -> Result<(), Box<dyn std::error::Error>> {
                     false
                 ) {
                     Ok(_) => {},
-                    Err(e) => return Err(Box::new(InitError::new(e)))
+                    Err(e) => {
+
+                        println!("The main branch already exists, but we could not checkout to it. Error: {}", e);
+
+                        return Err(Box::new(InitError::new(e)));
+
+                    }
                 }
 
             }
 
         }
     }
-    
-    return Err(Box::new(InitError::new(String::from("Function not finished yet!"))))
+
+    Ok(())
 
 }
 
+fn store_branch_names (
+    feature_branch_name: &str,
+    bugfix_branch_name: &str,
+    hotfix_branch_name: &str,
+    release_branch_name: &str,
+    develop_branch_name: &str,
+    main_branch_name: &str
+) -> Result<(), Box<InitError>> {
+
+    // 1. Storing the feature branch "prefix".
+    match Store::add(FEATURE_BRANCH_NAME_KEY, feature_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(Box::new(InitError::new(e)))
+    }
+
+    // 2. Storing the bugfix branch "prefix".
+    match Store::add(BUGFIX_BRANCH_NAME_KEY, bugfix_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(Box::new(InitError::new(e)))
+    }
+
+    // 3. Storing the hotfix branch "prefix".
+    match Store::add(HOTFIX_BRANCH_NAME_KEY, hotfix_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(Box::new(InitError::new(e)))
+    }
+
+    // 4. Storing the release branch "prefix".
+    match Store::add(RELEASE_BRANCH_NAME_KEY, release_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(Box::new(InitError::new(e)))
+    }
+
+    // 5. Storing the develop branch name.
+    match Store::add(DEVELOP_BRANCH_NAME_KEY, develop_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(Box::new(InitError::new(e)))
+    }
+
+    // 6. Storing the main branch name.
+    match Store::add(MAIN_BRANCH_NAME_KEY, main_branch_name) {
+        Ok(_) => {},
+        Err(e) => return Err(Box::new(InitError::new(e)))
+    }
+
+    Ok(())
+
+}
--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,14 @@ mod flow;
 
 use std::process;
 
-use config::file::{
-    create_config_file,
-    contains_config_file,
-    get_config_value,
-    MAIN_BRANCH_NAME_KEY,
-};
-
 use config::error::{INIT_ERROR_CODE};
 
-use command::gitv2::{GitV2};
+use flow::init::{Script};
 
-use flow::branch::{Branch};
+fn using_store_strategy () {
 
-fn main() {
-    println!("##############");
-    println!("git lab plugin");
-    println!("##############\r\n");
-    if !contains_config_file() {
-        match create_config_file() {
+    if ! Script::is_initiated() {
+        match Script::create() {
             Ok(_) => {},
             Err(e) => {
                 eprintln!("{}", e);
@@ -32,27 +21,18 @@ fn main() {
             }
         }
     } else {
-        println!("{}", "Configuration file \".git/lab.conf\" already exists");
-        println!("Main branch: {}", get_config_value(MAIN_BRANCH_NAME_KEY).unwrap());
-        println!("Develop branch: {}", get_config_value("DEVELOP_BRANCH_NAME").unwrap());
-        println!("Release branch: {}", get_config_value("RELEASE_BRANCH_NAME").unwrap());
-        println!("Bugfix branch: {}", get_config_value("BUGFIX_BRANCH_NAME").unwrap());
-        println!("Hotfix branch: {}", get_config_value("HOTFIX_BRANCH_NAME").unwrap());
-        println!("Feature branch: {}", get_config_value("FEATURE_BRANCH_NAME").unwrap());
-
-        println!("[DEBUG] Testing GitV2");
-
-        let branch = Branch::Bugfix("AN_ISSUE".to_string());
-
-        match branch.source() {
-            Ok(branches) => {
-                println!("[DEBUG] Source branches: {:?}", branches);
-            },
-            Err(e) => {
-                println!("[DEBUG] Error: {}", e);
-            }
-        }
-
-
+        println!("{}", "Lab Flow is already initiated");
+        Script::show();
     }
+
+}
+
+fn main() {
+
+    println!("#########");
+    println!("Lab Flow");
+    println!("#########\r\n");
+
+    using_store_strategy();
+    
 }
